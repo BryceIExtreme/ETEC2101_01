8e16cf15921dcf1eeebf06ffdb7caec9f006dfb6
BryceIExtreme <166551072+BryceIExtreme@users.noreply.github.com>
2025-04-27 23:49:27 -0400

    Hashmap lab done after finishing documentation.

--- include/ssuds/unordered_hashmap.h
+++ include/ssuds/unordered_hashmap.h
@@ -6,27 +6,47 @@
 
 namespace ssuds
 {
+    /// <summary>
+    /// This kind of structure takes a value V and associates it with a key K that we use a has to essentially jumble
+    /// </summary>
+    /// <typeparam name="K"></typeparam>
+    /// <typeparam name="V"></typeparam>
     template <typename K, typename V>
     class UnorderedHashMap
     {
     private:
         struct KeyValuePair {
+
+            /// <summary>
+            /// Value and key kinda go together, a key is tied to a value, 
+            /// a value is tied to a key 
+            /// and they cannot exist without each other
+            /// </summary>
             K key;
             V value;
-            bool is_occupied;
+            bool is_occupied; //Tells us whether a slot is occupado
 
             KeyValuePair() : is_occupied(false) {}
         };
 
-        KeyValuePair* table;
-        unsigned int mCapacity;
-        unsigned int mSize;
-        float load_factor;
+        KeyValuePair* table; //The table storing key-value pairs
+        unsigned int mCapacity; //The max ammount allowed in the hash table before we grow it
+        unsigned int mSize; //the number of elements in the table
+        float load_factor; //Maximum load factor before resizing
 
+        /// <summary>
+        /// The hash computer where we give it a ket and we get back a hash value
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         unsigned int hash(const K& key) const {
             return std::hash<K>{}(key) % mCapacity;
         }
 
+        /// <summary>
+        /// If we reach the max amount of capacity we use this to instantly increase out space by double after creating a new table
+        /// and copying all the information over
+        /// </summary>
         void grow() {
             mCapacity *= 2;
             KeyValuePair* new_table = new KeyValuePair[mCapacity];
@@ -46,17 +66,32 @@ namespace ssuds
         }
 
     public:
+
+        /// <summary>
+        /// Our initial setup function
+        /// </summary>
+        /// <param name="initial_mCapacity"></param>
+        /// <param name="load_factor"></param>
         UnorderedHashMap(unsigned int initial_mCapacity = 16, float load_factor = 0.75)
             : mCapacity(initial_mCapacity), mSize(0), load_factor(load_factor)
         {
             table = new KeyValuePair[mCapacity];
         }
 
+        /// <summary>
+        /// The standard deconstructor which destroys every part of the table
+        /// </summary>
         ~UnorderedHashMap()
         {
             delete[] table;
         }
 
+        /// <summary>
+        /// Our friendly stream operator which allows to actually print the values in the map 
+        /// </summary>
+        /// <param name="os"></param>
+        /// <param name="map"></param>
+        /// <returns></returns>
         friend std::ostream& operator<<(std::ostream& os, const UnorderedHashMap& map)
         {
             os << "{";
@@ -74,6 +109,13 @@ namespace ssuds
             return os;
         }
 
+
+        /// <summary>
+        /// Accesses or inserts a value with a given key.
+        /// if the key doesnt exist then we create a new value with the default
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         V& operator[](const K& key)
         {
             if (mSize >= mCapacity * load_factor)
@@ -97,7 +139,11 @@ namespace ssuds
             return table[index].value;
         }
 
-
+        /// <summary>
+        /// Checks if the table contains the value by the key given that alligns with it
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         bool contains(const K& key) const
         {
             unsigned int index = hash(key);
@@ -112,6 +158,13 @@ namespace ssuds
             return false;
         }
 
+        /// <summary>
+        /// Removes a key-value pair from the map via an index we feed it
+        /// if it does remove it, we get back true
+        /// if we dont find it, then we get false
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         bool remove(const K& key)
         {
             unsigned int index = hash(key);
@@ -128,26 +181,50 @@ namespace ssuds
             return false;
         }
 
+        /// <summary>
+        /// Gets the number of elements in the hash map
+        /// </summary>
+        /// <returns></returns>
         unsigned int get_size() const {
             return mSize;
         }
 
         class Iterator {
         private:
-            const UnorderedHashMap& map;
-            unsigned int index;
+            const UnorderedHashMap& map; //Hashmap being traversed
+            unsigned int index; //Current index in the hash table
 
         public:
+
+            /// <summary>
+            /// Like with all other labs, we needed an iterator to manually go through values in a glob of data.
+            /// We also need a default constructor to actually make an iterator
+            /// </summary>
+            /// <param name="map"></param>
+            /// <param name="index"></param>
             Iterator(const UnorderedHashMap& map, unsigned int index) : map(map), index(index) {}
 
+            /// <summary>
+            /// Simple check if an iterator and another arent equal
+            /// </summary>
+            /// <param name="other"></param>
+            /// <returns></returns>
             bool operator!=(const Iterator& other) const {
                 return index != other.index;
             }
 
+            /// <summary>
+            /// Dereferencing Operator for our iterator to get the current key-value
+            /// </summary>
+            /// <returns></returns>
             const KeyValuePair& operator*() const {
                 return map.table[index];
             }
 
+            /// <summary>
+            /// Advances the iterator by one slot and gives us a new iterator to said spot
+            /// </summary>
+            /// <returns></returns>
             Iterator& operator++() {
                 do {
                     ++index;
@@ -156,6 +233,10 @@ namespace ssuds
             }
         };
 
+        /// <summary>
+        /// Gets an iterator to the first actually occupied slot in the hash map, not just the first slot
+        /// </summary>
+        /// <returns></returns>
         Iterator begin() const {
             unsigned int index = 0;
             while (index < mCapacity && !table[index].is_occupied) {
@@ -164,6 +245,10 @@ namespace ssuds
             return Iterator(*this, index);
         }
 
+        /// <summary>
+        /// Gets an iterator to the end of the map
+        /// </summary>
+        /// <returns>An iterator to the last value of the hash map</returns>
         Iterator end() const
         {
             return Iterator(*this, mCapacity);
